10b0fa24a05e60d7b612003a184bf5a6280f6575
AurelijaZuba <auryte00@hotmail.com>
2019-08-27 12:04:11 +0100

    passing test for value of one

--- src/fizz-buzz/__tests__/fizzbuzz.test.ts
+++ src/fizz-buzz/__tests__/fizzbuzz.test.ts
@@ -3,6 +3,6 @@ import {FizzBuzz} from "../fizzbuzz";
 describe("fizz buzz", () => {
     it("should return number", () => {
         const fizzbuzz = new FizzBuzz();
-        expect(fizzbuzz.run(0)).toBe(1);
+        expect(fizzbuzz.run(1)).toBe(1);
     });
 });
